d972462c909c8e56becf3286f391bcf344ba1ca9
Anosy0 <lucas.theodore2004@gmail.com>
2025-02-18 20:31:58 -0500

    Add : BottomNavBar

---
+++ app/src/main/java/ca/uqac/tp_informatique_mobile_8inf257/presentation/NavItem.kt
@@ -0,0 +1,8 @@
+package ca.uqac.tp_informatique_mobile_8inf257.presentation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavItem (
+    val title: String,
+    val icon : ImageVector
+)
